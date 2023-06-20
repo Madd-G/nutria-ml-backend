f8bcdff347aa68029cfc97f74c24b9d8ee3edc3a
Madd-G <akhmadnuralamsyah@gmail.com>
2023-06-20 10:40:45 +0700

    only ML

--- schemas/fru_veg.py
+++ schemas/fru_veg.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
-
-
-class FruVeg(BaseModel):
-    name: str
-    description: str
-    category: str
-    nutrients: str
-    benefits: str
+# from pydantic import BaseModel
+#
+#
+# class FruVeg(BaseModel):
+#     name: str
+#     description: str
+#     category: str
+#     nutrients: str
+#     benefits: str
--- schemas/vegetable.py
+++ schemas/vegetable.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
-
-
-class Vegetable(BaseModel):
-    name: str
-    description: str
-    category: str
-    nutrients: str
-    benefits: str
+# from pydantic import BaseModel
+#
+#
+# class Vegetable(BaseModel):
+#     name: str
+#     description: str
+#     category: str
+#     nutrients: str
+#     benefits: str
